a938c507fe4dc07603b2beb47ddd9b231ab045e8
AlbertLiu <457566268@qq.com>
2023-10-12 16:39:54 -0400

    Finish JSP A2-3

--- JavaScriptProgramming/chapter07/assignments/Assignment2-Files/convert_temps/convert_temp.js
+++ JavaScriptProgramming/chapter07/assignments/Assignment2-Files/convert_temps/convert_temp.js
@@ -8,14 +8,32 @@ const calculateCelsius = temp => (temp-32) * 5/9;
 const calculateFahrenheit = temp => temp * 9/5 + 32;
 
 const toggleDisplay = (label1Text, label2Text) => {
+	$("#degree_label_1").textContent = label1Text;
+	$("#degree_label_2").textContent = label2Text;
+	$("#degrees_computed").value = "";
 
+	// move focus and select
+	$("#degrees_entered").select();
 }
 
 /****************************
 *  event handler functions  *
 *****************************/
 const convertTemp = () => {   
-
+	const value = $("#degrees_entered").value;
+	if (isNaN(value)) {
+		$("#message").textContent = "You must enter a valid number for degrees";	
+		return;
+	} else {
+		$("#message").textContent = "";	
+	}
+	let result;
+	if ($("#to_celsius").checked) {
+		result = calculateCelsius(value);
+	} else {
+		result = calculateFahrenheit(value);
+	}
+	$("#degrees_computed").value = Math.floor(result);
 };
 
 const toCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");
