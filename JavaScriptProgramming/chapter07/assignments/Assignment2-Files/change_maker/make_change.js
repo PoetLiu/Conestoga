b1c29c7d984f7f63bfa8a2e9b690febb719f5928
AlbertLiu <457566268@qq.com>
2023-10-12 16:11:35 -0400

    Finish JSP A2-2

--- JavaScriptProgramming/chapter07/assignments/Assignment2-Files/change_maker/make_change.js
+++ JavaScriptProgramming/chapter07/assignments/Assignment2-Files/change_maker/make_change.js
@@ -1,2 +1,30 @@
 const $ = selector => document.querySelector(selector);
 
+const processEntry = () => {
+    const value = parseInt($("#cents").value);
+    if (value >= 0 && value <= 99) {
+        makeChange(value);
+        $("#error").textContent = "";
+    } else {
+        $("#error").textContent = "amount must be a number between 0 and 99";
+    }
+}
+
+/* value of change and id of html elements. */
+const changes = [
+    { value: 25, id: "quarters" }, 
+    { value: 10, id: "dimes" }, 
+    { value: 5, id: "nickels" }, 
+    { value: 1, id: "pennies" }
+];
+const makeChange = (value) => {
+    for (let i = 0; i < changes.length; i++) {
+        const num = Math.floor(value / changes[i].value);
+        $("#" + changes[i].id).value = num;
+        value %= changes[i].value;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#calculate").addEventListener("click", processEntry);
+});
