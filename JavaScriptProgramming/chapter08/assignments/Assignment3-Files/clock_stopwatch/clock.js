62aef160c52383b7e5b49e01c6f8cba1c5b31c5e
AlbertLiu <457566268@qq.com>
2023-10-30 21:45:59 -0400

    Finish JSP assignment3-part2

--- JavaScriptProgramming/chapter08/assignments/Assignment3-Files/clock_stopwatch/clock.js
+++ JavaScriptProgramming/chapter08/assignments/Assignment3-Files/clock_stopwatch/clock.js
@@ -3,9 +3,11 @@ const $ = selector => document.querySelector(selector);
 
 const padSingleDigit = num => num.toString().padStart(2, "0");
 
+const padToThreeDigit = num => num.toString().padStart(3, "0");
+
 const displayCurrentTime = () => {
     const now = new Date();
-    const hours = now.getHours();
+    let hours = now.getHours();
     let ampm = "AM"; // set default value
     
     // correct hours and AM/PM value for display
@@ -38,39 +40,73 @@ let elapsedMilliseconds = 0;
 
 const tickStopwatch = () => {    
     // increment milliseconds by 10 milliseconds
+    elapsedMilliseconds += 10;
     
     // if milliseconds total 1000, increment seconds by one and reset milliseconds to zero
+    if (elapsedMilliseconds == 1000) {
+        elapsedSeconds++;
+        elapsedMilliseconds = 0;
+    }
     
     // if seconds total 60, increment minutes by one and reset seconds to zero
+    if (elapsedSeconds == 60) {
+        elapsedMinutes++;
+        elapsedSeconds = 0;
+    }
     
     //display new stopwatch time
-    
+    updateStopwatch();
 };
+
+const updateStopwatch = () => {
+    $("#s_minutes").textContent = padSingleDigit(elapsedMinutes);
+    $("#s_seconds").textContent = padSingleDigit(elapsedSeconds);
+    $("#s_ms").textContent = padToThreeDigit(elapsedMilliseconds);
+}
 
 // event handler functions
 const startStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
         
+    // if it has been started, ignore this evt.
+    if (stopwatchTimer != null) {
+        return;
+    }
     // do first tick of stop watch and then set interval timer to tick 
     // stop watch every 10 milliseconds. Store timer object in stopwatchTimer 
     // variable so next two functions can stop timer.
+    tickStopwatch();
+    stopwatchTimer = setInterval(tickStopwatch, 10);
     
 };
 
+const stopStopwatchTimer = () => {
+    clearInterval(stopwatchTimer);
+    stopwatchTimer = null;
+}
+
 const stopStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
         
     // stop timer
+    stopStopwatchTimer();
     
 };
 
 const resetStopwatch = evt => {
     // prevent default action of link
+    evt.preventDefault();
         
     // stop timer
+    stopStopwatchTimer();
         
     // reset elapsed variables and clear stopwatch display
-    
+    elapsedMinutes = 0;
+    elapsedSeconds = 0;
+    elapsedMilliseconds = 0; 
+    updateStopwatch();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -81,4 +117,8 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(displayCurrentTime, 1000);
 	
 	// set up stopwatch event handlers
+    $("#start").addEventListener("click", startStopwatch);
+    $("#stop").addEventListener("click", stopStopwatch);
+    $("#reset").addEventListener("click", resetStopwatch);
+
 });
