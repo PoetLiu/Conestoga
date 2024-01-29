5f38ec99c22599ef8c9afac6ffeb0a52936ee071
AlbertLiu <457566268@qq.com>
2024-01-29 14:43:33 -0500

    Add DB lab3

---
+++ Semester2/AdvancedDatabaseDesign/week3/Lab3_Peng.sql
@@ -0,0 +1,37 @@
+use AdvDatabase;
+
+-- Task-1
+DROP TABLE IF EXISTS bankaccounts;
+CREATE TABLE bankaccounts(accountno varchar(20) PRIMARY
+KEY NOT NULL, funds decimal(8,2));
+
+INSERT INTO bankaccounts VALUES("ACC1", 1000);
+INSERT INTO bankaccounts VALUES("ACC2", 1000);
+
+START TRANSACTION; -- statement1
+UPDATE bankaccounts SET funds=funds-100 WHERE
+accountno='ACC1'; -- statement2
+UPDATE bankaccounts SET funds=funds+100 WHERE
+accountno='ACC2'; -- statement3
+COMMIT; -- statement4
+
+START TRANSACTION; -- statement1
+UPDATE bankaccounts SET funds=funds-100 WHERE
+accountno='ACC1'; -- statement2
+UPDATE bankaccounts SET funds=funds+100 WHERE
+accountno='ACC2'; -- statement3
+
+ROLLBACK; -- statement4
+
+SELECT * FROM AdvDatabase.bankaccounts;
+SHOW VARIABLES LIKE 'AUTOCOMMIT';
+
+-- Task-2
+SET autocommit = 0;
+DELETE from products;
+SELECT * FROM products;
+
+SET autocommit = 1;
+DELETE from products;
+
+DELETE from products;
